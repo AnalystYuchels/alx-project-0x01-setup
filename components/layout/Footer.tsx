47ba693d83f4b2845f040577c973258bfdaf278c
AnalystYuchels <uche4xploit@gmail.com>
2025-07-18 20:14:24 +0100

    Organized project inside directory

--- components/layout/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer: React.FC = () => {
-  return (
-    <footer className="w-full p-4 bg-gray-800 text-white text-center mt-auto">
-      <p>&copy; 2025 My App. All rights reserved.</p>
-    </footer>
-  )
-}
-
-export default Footer;
--- components/layout/Header.tsx
+++
@@ -1,9 +0,0 @@
-const Header: React.FC = () => {
-  return (
-    <header className="w-full p-4 bg-gray-900 text-white text-center">
-      <h1 className="text-3xl">My App Header</h1>
-    </header>
-  )
-}
-
-export default Header;
